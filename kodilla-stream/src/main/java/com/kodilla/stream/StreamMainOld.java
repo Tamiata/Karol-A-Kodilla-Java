4037f85d88a2431f473d21ed37c9c4d1bd53aebb
Karol Androsz <karol@androsz.pl>
2021-06-02 04:02:16 +0200

    Task 7.3 Forum User Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMainOld.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMainOld.java
@@ -35,3 +35,31 @@ public class StreamMainOld {
 
     }
 }
+
+//  public static void main(String[] args) {
+//       BookDirectory theBookDirectory = new BookDirectory();
+//
+//       Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+//              .filter(book -> book.getYearOfPublication() > 2005)
+//              .collect(Collectors.toMap(Book::getSignature, book -> book));
+//
+//       System.out.println("# elements: " + theResultMapOfBooks.size());
+//
+//       theResultMapOfBooks.entrySet().stream()
+//              .map(entry -> entry.getKey() + ": " + entry.getValue())
+//              .forEach(System.out::println);
+//  }
+//}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  public static void main(String[] args) {
+//        BookDirectory theBookDirectory = new BookDirectory();
+//
+//        String theResultStringOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .map(Book::toString)
+//                .collect(Collectors.joining(",\n","<<",">>"));
+//
+//        System.out.println(theResultStringOfBooks);
+//  }
+//}
